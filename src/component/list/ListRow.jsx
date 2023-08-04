3a667ebd3b4bdfdfd3d893fa4ab7dde6e285176c
Dandu Madhukar Reddy <104265607+Madhukar-Reddy2002@users.noreply.github.com>
2023-08-04 13:52:53 +0530

    Update ListRow.jsx

--- src/component/list/ListRow.jsx
+++ src/component/list/ListRow.jsx
@@ -1,7 +1,7 @@
 import styles from "./ListRow.module.css";
 
-const ListCell = ({ children }) => {
-  return <tr className={styles.cell}>{children}</tr>;
+const ListCell = ({ children, fetchid }) => {
+  return <tr className={styles.cell} onClick={()=>{ fetchid(children[0].props.children)}}>{children}</tr>;
 };
 
 export default ListCell;
